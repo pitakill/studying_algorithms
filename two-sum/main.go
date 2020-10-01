30685480ead88b0e7f4d0cb317078c402922073a
Leopoldo Caballero <git@pitakill.net>
2020-09-30 21:36:55 -0500

    https://leetcode.com/problems/two-sum

---
+++ two-sum/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Println(twoSum(input, target))
+
+	input = []int{3, 2, 4}
+	target = 6
+	fmt.Println(twoSum(input, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	if len(nums) == 2 {
+		return []int{0, 1}
+	}
+
+	output := make([]int, 2)
+
+	for i, v := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if target-v == nums[j] {
+				output = append([]int{}, i, j)
+			}
+		}
+	}
+
+	return output
+}
