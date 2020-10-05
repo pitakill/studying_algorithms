620345c219e30a96a1256eb00788000847c5ab31
Ernesto <ernestociencias@yahoo.com.mx>
2020-10-05 08:32:59 -0500

    No se pudo...

--- two-sum/main.go
+++ two-sum/main.go
@@ -7,18 +7,16 @@ func main() {
 	target := 9
 	fmt.Println(twoSum(input, target))
 
-	input = []int{3, 2, 4}
-	target = 6
-	fmt.Println(twoSum(input, target))
+	input1 := []int{3, 2, 4}
+	target1 := 6
+	fmt.Println(twoSum(input1, target1))
 }
 
 func twoSum(nums []int, target int) []int {
 	if len(nums) == 2 {
 		return []int{0, 1}
 	}
-
 	output := make([]int, 2)
-
 	for i, v := range nums {
 		for j := i + 1; j < len(nums); j++ {
 			if target-v == nums[j] {
@@ -26,6 +24,5 @@ func twoSum(nums []int, target int) []int {
 			}
 		}
 	}
-
 	return output
 }
