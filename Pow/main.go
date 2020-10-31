df3c38c25d404b892528334a6f11d4d3f2d9eb94
Ernesto <ernestociencias@yahoo.com.mx>
2020-10-31 17:57:33 -0600

    Otro intento fallido...

---
+++ Pow/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x float64
+	var n int
+
+	x = 2.00000
+	n = 10
+	fmt.Println(Pow(x, n))
+
+	x = 2.10000
+	n = 3
+	fmt.Println(Pow(x, n))
+
+	x = 2.00000
+	n = -2
+	fmt.Println(Pow(x, n))
+}
+
+func Pow(x float64, n int) float64 {
+	var E int
+	var y float64
+	E = 1
+
+	if n == 0 {
+		y = 1
+	}
+	if n > 0{
+		x = x*Pow(x,n-1)
+		y = x
+		}
+		if n < 0{
+			E = -1
+			n = n*E
+			x = x*Pow(x,n-1)
+			y = 1 / x
+	}
+	return y
+}
