94a254c6848864c76cca0bede1d7260af0574ac3
Shino <ernestociencias@yahoo.com.mx>
2020-12-30 19:29:20 -0600

    Al parecer, se logró el objetivo
    Quizá no del modo correcto, pero parece que ya salió :D

--- Pendiente/main.go
+++ Pendiente/main.go
@@ -1,4 +1,4 @@
-	package main
+package main
 
 import (
 	"fmt"
@@ -6,49 +6,58 @@ import (
 )
 
 func main() {
-	fmt.Println(paso1("8"))
-	fmt.Println(paso1("3"))
-	fmt.Println(paso1("22"))
-	fmt.Println(paso1("112233"))
-	fmt.Println(paso1("111223"))
-	fmt.Println(paso2("111223"))
+	fmt.Println(say(1))
+	fmt.Println(say(2))
+	fmt.Println(say(3))
+	fmt.Println(say(4))
+	fmt.Println(say(5))
+	fmt.Println(say(6))
+	fmt.Println(say(7))
+	fmt.Println(say(8))
 }
 
-func paso1(s string) (int, string, string){
+func paso1(s string) (string, string){
 	cantidad := 0
-	subs := s[0:]
+	subs := s
 
-if len(s) == 1{
+if len(s) == 0 {
+	return "", ""
+}
+if len(s) == 1 {
 	cantidad = 1
 	subs = ""
-} else {
-	for i := 0; i < len(s); i++ {
-		if s[0] == s[i] {
-		cantidad = i+1
-		subs = s[i+1:]
+}
+if 1 < len(s){
+	for i := 1; i < len(s); i++ {
+		if s[0] != s[i] {
+			cantidad = i
+			subs = s[i:]
+			break
+		} else {
+		cantidad = len(s)
+		subs = ""
 	}
 }
 }
-return cantidad, string(s[0]), subs
+cuantos := strconv.Itoa(cantidad)+string(s[0])
+return cuantos, subs
 }
 
 func paso2(s string) (string){
-	w, y, z := paso1(s)
-	x := strconv.Itoa(w)
+	y, z := paso1(s)
 	cola := ""
-	d, b, c := paso1(z)
-	a := strconv.Itoa(d)
-	if 0 < len(z){
-		cola = paso2(c)
-	}
+	a, b := paso1(z)
 	if 0 == len(z){
 		cola = a + b
 	}
-	return x + y + cola
+	if 0 < len(z){
+		cola = a + paso2(b)
+	}
+	return y + cola
 }
 
 func say(n int) (string){
-	if n == 1{
+	if n == 1 {
 		return "1"
 	} else {
 		return paso2(say(n-1))
